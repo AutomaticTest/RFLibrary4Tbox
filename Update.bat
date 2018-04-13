4bd786ecd9b900e6c2b0a58619fc77d39b4d045f
Clive Lau <liuxusheng@auto-link.com.cn>
2018-04-13 17:42:59 +0800

    feat: update .bat script for updating RIDE program

--- Update.bat
+++ Update.bat
@@ -2,31 +2,24 @@
 
 echo This bat script is used for update program
 
+set scandisk=c d e f g h i
+
 rem 更新ShecklockLibrary程序
 git fetch -p
 git merge origin/master
 echo ---Done to update ShecklockLibrary---
 
-rem 更新TBoxLibrary程序
-for /r c:\ %%d in (Python27) do (
-    if exist %%d (
-	    cd %%d\Lib\site-packages\TBoxLibrary
-	    git clean -df
+for %%tmp in (%scandisk%) do (
+    if exist %%tmp:\Python27\Lib\site-packages (
+        rem 更新TBoxLibrary程序
+        cd %%tmp:\Python27\Lib\site-packages\TBoxLibrary
+        git clean -df
 		git fetch -p
 		git reset --hard origin/master
-		goto :exit
-	)
-)
-for /r d:\ %%d in (Python27) do (
-    if exist %%d (
-	    cd %%d\Lib\site-packages\TBoxLibrary
-	    git clean -df
-		git fetch -p
-		git reset --hard origin/master
-		goto :exit
-	)
+
+		rem 更新RIDE程序
+		xcopy %%tmp:\Python27\Lib\site-packages\TBoxLibrary\Patches\RIDE\testrunnerplugin.py %%tmp:\Python27\Lib\site-packages\robotide\contrib\testrunner\testrunnerplugin.py
+    )
 )
 
-:exit
-echo ---Done to update TBoxLibrary---
 @pause
